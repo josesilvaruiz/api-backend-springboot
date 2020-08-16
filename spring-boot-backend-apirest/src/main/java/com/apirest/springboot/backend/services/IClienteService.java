6a09619e3264e68e15b9d77da60e059e888dcbff
Jose Antonio Silva Ruiz <joserota12@hotmail.com>
2020-08-16 13:56:45 +0200

    First Commit

---
+++ spring-boot-backend-apirest/src/main/java/com/apirest/springboot/backend/services/IClienteService.java
@@ -0,0 +1,22 @@
+package com.apirest.springboot.backend.services;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.apirest.springboot.backend.models.entity.Cliente;
+
+public interface IClienteService {
+
+	public List<Cliente> findAll();
+	
+	public Page<Cliente> findAll(Pageable pageable);
+	
+	public Cliente findById(Long id);
+	
+	public Cliente save(Cliente cliente);
+	
+	public void delete(Long id);
+
+}
